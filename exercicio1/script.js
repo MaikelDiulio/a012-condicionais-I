f0800d976bcb3b63a82600b14d462c167db9f37d
maikeldiulio <maikeltecba@gmail.com>
2022-09-06 16:15:40 -0300

    exercícios Condicionais comcluidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,13 @@
-const num = //seu numero aqui
+const num = Number(prompt("digite um número"))
 
-if(){
-    console.log("sua mensagem aqui");
+if(num %2 === 0){
+    console.log("o número é par");
 }
+
+
+
+
+
+// Crie um código que receba uma const numérica qualquer. Crie um `if` 
+// para verificar se o número guardado na const é **par**.
+// Caso seja, imprima no console a mensagem: "o número é par".
